659beaa87732b0154aab0bb328c529f3aec3846a
Janez Lapajne <lapajne.janez@gmail.com>
2022-01-31 16:41:35 +0100

    added grid search for search of best params

--- scripts/features_selection.py
+++ scripts/features_selection.py
@@ -10,14 +10,16 @@ from skmultiflow.data import DataStream
 from skmultiflow.meta import AdaptiveRandomForestRegressor
 from xgboost import XGBRegressor
 
+import utils.helpers as helpers
 import utils.utils as utils
 from data_management.data import Data
-import utils.helpers as helpers
 
 
-def select_features(target_label, no_hist_days, no_hist_weeks, begin_test_date, scale_data, threshold, save_name_pkl):
+def select_features(target_label, no_hist_days, no_hist_weeks, begin_test_date,
+                                    scale_data, threshold, save_name_pkl, plot):
 
     ### Import data
+    begin_test_date = "2025-11-06"
     data = Data(
         no_hist_days=no_hist_days,
         no_hist_weeks=no_hist_weeks,
@@ -43,21 +45,25 @@ def select_features(target_label, no_hist_days, no_hist_weeks, begin_test_date, 
     feats_names_selected = []
     for feat_n, feat_i in zip(feats_names, feats_importances):
         if feat_i > threshold:
-            print('%.5f ->  %s' % (feat_i, feat_n))
+            if plot:
+                print('%.5f ->  %s' % (feat_i, feat_n))
             feats_names_selected.append(feat_n)
 
     ### Plot feature importance
-    # plt.figure(figsize=[12,9])
-    # sns.barplot(x=feats_importances, y=feats_names)
-    # plt.show()
+    if plot:
+        plt.figure(figsize=[12,9])
+        sns.barplot(x=feats_importances, y=feats_names)
+        plt.show()
 
     ### Save selected features
     with open(os.path.join("output/features", save_name_pkl), "wb") as file:
         pickle.dump(feats_names_selected, file)
 
 
-def evaluate_model(target_label, no_hist_days, no_hist_weeks, begin_test_date, scale_data, threshold, save_name_pkl):
+def evaluate_model(target_label, no_hist_days, no_hist_weeks, begin_test_date,
+                                    scale_data, threshold, save_name_pkl, plot):
 
+    ### Load selected features
     with open(os.path.join("output/features", save_name_pkl), "rb") as file:
         selected_features = pickle.load(file)
 
@@ -74,7 +80,10 @@ def evaluate_model(target_label, no_hist_days, no_hist_weeks, begin_test_date, s
     X_train, y_train, X_test_t, y_test_t = data.get_data()
     stream = DataStream(X_test_t, y_test_t)
 
-    regr = AdaptiveRandomForestRegressor()
+    regr = AdaptiveRandomForestRegressor(random_state=1, drift_detection_method=None,
+                                                        warning_detection_method=None)
+    if X_train.shape[1] <= 1 :
+        return 1e10
     regr.fit(X_train, y_train)
 
     y_pred, y_test = [], []
@@ -88,39 +97,82 @@ def evaluate_model(target_label, no_hist_days, no_hist_weeks, begin_test_date, s
     y_pred = np.array(y_pred).flatten()
     y_test = np.array(y_test).flatten()
 
-
     rmse = mean_squared_error(y_test, y_pred, squared=False)
     # print(f"RMSE: {rmse}")
-    return rmse
 
     """Plot"""
-    # plt.figure()
-    # plt.plot(y_test, label="test")
-    # plt.plot(y_pred, label="pred")
-    # plt.legend()
-    # plt.show()
+    if plot:
+        plt.figure()
+        plt.plot(y_test, label="test")
+        plt.plot(y_pred, label="pred")
+        plt.legend()
+        plt.show()
+
+    return rmse
+
 
 
 if __name__ == "__main__":
 
     params = {
         "target_label" : "new_cases",
-        "no_hist_days" : 15,
-        "no_hist_weeks" : 5,
-        "begin_test_date" : "2025-11-06",
+        "no_hist_days" : 6,
+        "no_hist_weeks" : 0,
+        "begin_test_date" : "2021-11-14",
         "scale_data" : None,
         "threshold" : 0.1,
-        "save_name_pkl" : "d7w7t0001.pkl"
+        "save_name_pkl" : "d7w7t0001.pkl",
+        "plot": False
     }
 
     # select_features(**params)
     # params["begin_test_date"] = "2021-11-14"
     # # params["begin_test_date"] = "2020-03-07"  # uncomment for whole stream evaluation
-    # evaluate_model(**params)
+    # print(evaluate_model(**params))
 
     from itertools import product
+    from types import SimpleNamespace
+    def dict_configs(param_dict):
+        for vcomb in product(*param_dict.values()):
+            yield dict(zip(param_dict.keys(), vcomb))
 
-    def dict_configs(d):
-        for vcomb in product(*d.values()):
-            yield dict(zip(d.keys(), vcomb))
+    param_dict = {
+        "no_hist_days": [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
+        "no_hist_weeks": [0,1,2,3],
+        "threshold": [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16]
+    }
+
+    config_best = SimpleNamespace(rmse_best=1e10, params_best=None, rmse_list=[], params_list=[])
+    idx_all = len(list(product(*list(param_dict.values()))))
+
+    for idx, params_temp in enumerate(dict_configs(param_dict)):
+        print(f"ITER: {idx} / {idx_all}")
+        params["no_hist_days"] = params_temp["no_hist_days"]
+        params["no_hist_weeks"] = params_temp["no_hist_weeks"]
+        params["threshold"] = params_temp["threshold"]
+
+        select_features(**params)
+        rmse = evaluate_model(**params)
+
+        config_best.rmse_list.append(rmse)
+        config_best.params_list.append(params_temp)
+
+        if rmse < config_best.rmse_best:
+            config_best.rmse_best = rmse
+            config_best.params_best = params_temp
+            print("RMSE:", rmse, " params:", params_temp, "\n")
+
+    sort_idx = np.argsort(config_best.rmse_list)
+    config_best.rmse_list = np.array(config_best.rmse_list)[sort_idx]
+    config_best.params_list = np.array(config_best.params_list)[sort_idx]
+
+    for idx, (rmse, params) in enumerate(zip(config_best.rmse_list, config_best.params_list)):
+        print(rmse, params)
+        if idx > 40:
+            break
+
+    print("\nBest rmse:", config_best.rmse_best)
+    print("Best params:", config_best.params_best)
+
+
 
